d7f29b3e3a645b664540e7ca28874cfaefb342dd
Biswas <devashish.biswas@sap.com>
2020-04-29 10:57:20 +0530

    Support Multilanguage tweets using google translator and handling of emojis in tweet using emoji library

--- SentimentAnalysis _Multilanguge_Support.py
+++ SentimentAnalysis _Multilanguge_Support.py
@@ -1,13 +1,17 @@
 import sys,tweepy,csv,re
 from textblob import TextBlob
 import matplotlib.pyplot as plt
-
+from googletrans import Translator
+import emoji
 
 class SentimentAnalysis:
 
     def __init__(self):
         self.tweets = []
         self.tweetText = []
+        self.tweetsInEnglish = []
+        #self.translator = Translator()
+        self.allTweetsInEnglish = []
 
     def downloadAndProcessData(self):        
 
@@ -21,11 +25,9 @@ class SentimentAnalysis:
         # searching for tweets
         self.tweets = tweepy.Cursor(twitterAPI.search, q=searchTerm, lang = "de").items(noOfTweetsToProcess)
         
-        for status in self.tweets:
-            new_entry = []
-            status = status._json
-            if status['lang'] == 'en':
-                print('devashish biswas')
+        # Translate Non english tweets in English for sentiment Analysis
+        self.tweetsInEnglish = self.translateNonEnglishTweetsToEnglish(self.tweets)
+        
 
         # Open/create a file to append data to
         csvFile = open('twitterData.csv', 'a')
@@ -45,13 +47,15 @@ class SentimentAnalysis:
         neutral = 0
 
 
-        # iterating through tweets fetched
-        for tweet in self.tweets:
-            #Append to temp so that we can store in csv later. I use encode UTF-8
-            self.tweetText.append(self.cleanTweet(tweet.text).encode('utf-8'))
-            #print (tweet.text.translate(non_bmp_map))  
+        print(self.tweetsInEnglish)
+        # iterating through the translated tweets 
+        for tweet in self.tweetsInEnglish:
             print(tweet)
-            analysis = TextBlob(tweet.text)
+            #Append to temp so that we can store in csv later. I use encode UTF-8
+            self.tweetText.append(self.cleanTweet(tweet).encode('utf-8'))
+            #print (tweet.text.translate(non_bmp_map))  
+            
+            analysis = TextBlob(tweet)
             # print(analysis.sentiment)  # print tweet's polarity
             polarity += analysis.sentiment.polarity  # adding up polarities to find the average later
 
@@ -86,6 +90,8 @@ class SentimentAnalysis:
 
         # finding average reaction
         polarity = polarity / noOfTweetsToProcess
+        
+        print ("Polarity of report : " +str(polarity))
 
         # printing out data
         print("How people are reacting on " + searchTerm + " by analyzing " + str(noOfTweetsToProcess) + " tweets.")
@@ -107,7 +113,7 @@ class SentimentAnalysis:
         elif (polarity > -1 and polarity <= -0.6):
             print("Strongly Negative")
 
-        '''print()
+        print()
         print("Detailed Report: ")
         print(str(positive) + "% people thought it was positive")
         print(str(wpositive) + "% people thought it was weakly positive")
@@ -116,14 +122,42 @@ class SentimentAnalysis:
         print(str(wnegative) + "% people thought it was weakly negative")
         print(str(snegative) + "% people thought it was strongly negative")
         print(str(neutral) + "% people thought it was neutral")
-'''
-        #self.plotPieChart(positive, wpositive, spositive, negative, wnegative, snegative, neutral, searchTerm, noOfTweetsToProcess)
+
+        self.plotPieChart(positive, wpositive, spositive, negative, wnegative, snegative, neutral, searchTerm, noOfTweetsToProcess)
 
 
     def cleanTweet(self, tweet):
         # Remove Links, Special Characters etc from tweet
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w +:\ / \ / \S +)", " ", tweet).split())
     
+    def translateNonEnglishTweetsToEnglish(self, tweets):        
+        for status in tweets:
+            status=status._json
+            if self.checkTweetLanguage(status):
+                #print(status['text'])
+                translator = Translator()
+                
+                # Handling emojis in tweet before translating using googletrans library
+                str_demoji = emoji.demojize(status['text'])
+                
+                #translating non english tweets to english
+                tran=translator.translate(str_demoji,dest='en').text
+                
+                str_emoji = emoji.emojize(tran.replace(': ',':').capitalize())
+                print(str_emoji)
+                self.allTweetsInEnglish.append(str_emoji)
+                #self.allTweetsInEnglish.append(self.translator.translate(status['text']).text)
+
+            else:
+                self.allTweetsInEnglish.append(status['text'])
+        return self.allTweetsInEnglish
+            
+    
+    # function to check the tweet language
+    def checkTweetLanguage(self , status):
+        return status['lang'] != 'en'
+            
+    # function to Authenticate Twitter Application
     def twitterApplicationAuthentication(self):
         # Twitter Authentication for application
         consumerKey = 'Your Key Here'
@@ -139,6 +173,7 @@ class SentimentAnalysis:
         temp = 100 * float(part) / float(whole)
         return format(temp, '.2f')
 
+    # function to plot pie chart
     def plotPieChart(self, positive, wpositive, spositive, negative, wnegative, snegative, neutral, searchTerm, noOfTweetsToProcess):
         labels = ['Positive [' + str(positive) + '%]', 'Weakly Positive [' + str(wpositive) + '%]','Strongly Positive [' + str(spositive) + '%]', 'Neutral [' + str(neutral) + '%]',
                   'Negative [' + str(negative) + '%]', 'Weakly Negative [' + str(wnegative) + '%]', 'Strongly Negative [' + str(snegative) + '%]']
